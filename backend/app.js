951365174131e5160ab18ead692289c15c435869
adrian-swifter <milosffh@yahoo.com>
2022-06-01 22:34:29 +0200

    edded env variables

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
 const express = require("express");
+require("dotenv").config();
 const firebase = require("firebase-admin");
-const serviceAccount = require("./shoutout-efa62-firebase-adminsdk-2xcbz-44d6796157.json");
+// const serviceAccount = require("./shoutout-efa62-firebase-adminsdk-2xcbz-44d6796157.json");
 const moment = require("moment");
 const app = express();
+// firebase.initializeApp({
+//   credential: firebase.credential.cert(serviceAccount),
+//   storageBucket: "shoutout-efa62.appspot.com",
+// });
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
   storageBucket: "shoutout-efa62.appspot.com",
 });
-
+console.log(process.env);
 let poolEntriesDataArr = [];
 let users = [];
 let nextPoolEndTime = null;
